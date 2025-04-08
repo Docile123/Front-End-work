7fd9c275023e6c00167c3e46ce9d84d4885b3743
Docile123 <imbereyemasoo@gmail.com>
2025-04-08 15:34:50 +0200

    work submission

---
+++ index.js
@@ -0,0 +1,11 @@
+let names = document.querySelector("#name");
+let email = document.querySelector("#email");
+let password = document.querySelector("#password");
+let form = document.querySelector("#form");
+form.addEventListener("submit",(e)=>{
+   if(names.value==""||email.value==""||password.value==""){
+     e.preventDefault();
+     alert("Please all fields before submission.")
+    document.getElementById("warning").innerHTML="Please, All input fileds are required before submission.😒"
+   }
+})
